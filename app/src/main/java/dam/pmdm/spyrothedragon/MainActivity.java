bf2de0e691bbb34c885b0d373829fc7681adb4a3
apalseg91 <apalomequesegura@gmail.com>
2025-02-14 11:10:25 +0100

    Added: guide navigation and bottons

--- app/src/main/java/dam/pmdm/spyrothedragon/MainActivity.java
+++ app/src/main/java/dam/pmdm/spyrothedragon/MainActivity.java
@@ -1,86 +1,79 @@
 package dam.pmdm.spyrothedragon;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import dam.pmdm.spyrothedragon.databinding.ActivityMainBinding;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    NavController navController = null;
+    private NavController navController = null;
+    private int currentGuideStep = 1;
+    private static final int TOTAL_STEPS = 6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = dam.pmdm.spyrothedragon.databinding.ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        Fragment navHostFragment = getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
-        if (navHostFragment != null) {
-            navController = NavHostFragment.findNavController(navHostFragment);
-            NavigationUI.setupWithNavController(binding.navView, navController);
-            NavigationUI.setupActionBarWithNavController(this, navController);
-        }
-
+        navController = NavHostFragment.findNavController(getSupportFragmentManager().findFragmentById(R.id.navHostFragment));
+        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController);
         binding.navView.setOnItemSelectedListener(this::selectedBottomMenu);
 
         navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
             if (destination.getId() == R.id.navigation_characters ||
                     destination.getId() == R.id.navigation_worlds ||
                     destination.getId() == R.id.navigation_collectibles) {
-                // Para las pantallas de los tabs, no queremos que aparezca la flecha de atrás
                 getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-            }
-            else {
-                // Si se navega a una pantalla donde se desea mostrar la flecha de atrás, habilítala
+            } else {
                 getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             }
         });
 
+        showGuideStep(currentGuideStep);
     }
 
     private boolean selectedBottomMenu(@NonNull MenuItem menuItem) {
         if (menuItem.getItemId() == R.id.nav_characters)
             navController.navigate(R.id.navigation_characters);
-        else
-        if (menuItem.getItemId() == R.id.nav_worlds)
+        else if (menuItem.getItemId() == R.id.nav_worlds)
             navController.navigate(R.id.navigation_worlds);
         else
             navController.navigate(R.id.navigation_collectibles);
         return true;
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Infla el menú
         getMenuInflater().inflate(R.menu.about_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Gestiona el clic en el ítem de información
         if (item.getItemId() == R.id.action_info) {
-            showInfoDialog();  // Muestra el diálogo
+            showInfoDialog();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
     private void showInfoDialog() {
-        // Crear un diálogo de información
         new AlertDialog.Builder(this)
                 .setTitle(R.string.title_about)
                 .setMessage(R.string.text_about)
@@ -88,6 +81,58 @@ public class MainActivity extends AppCompatActivity {
                 .show();
     }
 
+    private void showGuideStep(int step) {
+        LayoutInflater inflater = getLayoutInflater();
+        FrameLayout guideContainer = findViewById(R.id.guideContainer);
+        guideContainer.removeAllViews();
 
+        int layoutResId = 0;
+        switch (step) {
+            case 1:
+                layoutResId = R.layout.guide_no1;
+                break;
+            case 2:
+                layoutResId = R.layout.guide_no2;
+                break;
+            case 3:
+                layoutResId = R.layout.guide_no3;
+                break;
+            case 4:
+                layoutResId = R.layout.guide_no4;
+                break;
+            case 5:
+                layoutResId = R.layout.guide_no5;
+                break;
+            case 6:
+                layoutResId = R.layout.guide_no6;
+                break;
+        }
 
+        if (layoutResId != 0) {
+            View guideView = inflater.inflate(layoutResId, guideContainer, false);
+            guideContainer.addView(guideView);
+            guideContainer.setVisibility(View.VISIBLE);
+
+            Button nextButton = guideView.findViewById(R.id.btnNextStep);
+            Button skipButton = guideView.findViewById(R.id.btnSkipGuide);
+
+            nextButton.setOnClickListener(v -> onNextGuideStep());
+            skipButton.setOnClickListener(v -> onSkipGuide());
+        }
+    }
+
+    public void onNextGuideStep() {
+        if (currentGuideStep < TOTAL_STEPS) {
+            currentGuideStep++;
+            showGuideStep(currentGuideStep);
+        }
+    }
+
+    public void onSkipGuide() {
+        currentGuideStep = 1;
+        FrameLayout guideContainer = findViewById(R.id.guideContainer);
+        guideContainer.removeAllViews();
+        guideContainer.setVisibility(View.GONE);
+    }
 }
+
