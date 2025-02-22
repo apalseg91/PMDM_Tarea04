21a5f33c49c7e8c3f5f0f2cc6e96fc67e88c7980
apalseg91 <apalomequesegura@gmail.com>
2025-02-22 16:08:58 +0100

    Changes: Strings add

--- app/src/main/java/dam/pmdm/spyrothedragon/ui/VideoEasterEggActivity.java
+++ app/src/main/java/dam/pmdm/spyrothedragon/ui/VideoEasterEggActivity.java
@@ -1,10 +1,8 @@
 package dam.pmdm.spyrothedragon.ui;
 
-import android.content.Intent;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
+
 import android.widget.MediaController;
 import android.widget.VideoView;
 
